a278dcf7db2e2744d9849d3e818be9ea38e12df8
Krishna Kumar <149694348+KRISHNAKUMARPS2002@users.noreply.github.com>
2025-07-24 16:51:37 +0530

    done

--- omega/settings.py
+++ omega/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'api' / 'static']
 
+STATIC_ROOT = '/var/www/test-app/staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
